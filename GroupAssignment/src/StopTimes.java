9bb76fd971c988cddacd7bef9535f928158c52aa
swordsr <67106477+swordsr@users.noreply.github.com>
2021-04-20 15:01:29 +0100

    Completed shortest path using Dijkstra

---
+++ GroupAssignment/src/StopTimes.java
@@ -0,0 +1,119 @@
+import java.time.LocalTime;
+
+
+public class StopTimes {
+
+    // The data stored from stop_times.txt
+    private int tripId;
+    private LocalTime arrivalTime;
+    private LocalTime departureTime;
+    private int stopId;
+    private int stopSequence;
+    private int pickUpType;
+    private int dropOffType;
+    private double distance;
+
+    /**
+     * Constructor for StopTimes
+     * @param inputLine: A line from the stop_times.txt file. trip_id,arrival_time,departure_time,stop_id,stop_sequence,
+     *                 stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
+     */
+    public StopTimes(String inputLine){
+        // split the line into its individual parameters.
+        String[] inputArray = inputLine.split(",");
+
+        // assign the data to variables
+         try{
+             this.tripId = Integer.parseInt(inputArray[0]);
+         }
+         catch (Exception e){
+             this.tripId = -1;
+         }
+
+        try{
+            this.arrivalTime = LocalTime.parse(inputArray[1]);
+        }
+        catch (Exception e){
+            this.arrivalTime = null;
+        }
+
+        try{
+            this.departureTime = LocalTime.parse(inputArray[2]);
+        }
+        catch (Exception e){
+            this.departureTime = null;
+        }
+
+        try{
+            this.stopId = Integer.parseInt(inputArray[3]);
+        }
+        catch (Exception e){
+            this.stopId = -1;
+        }
+
+        try{
+            this.stopSequence = Integer.parseInt(inputArray[4]);
+        }
+        catch (Exception e){
+            this.stopSequence = -1;
+        }
+
+        try{
+            this.pickUpType = Integer.parseInt(inputArray[6]);
+        }
+        catch (Exception e){
+            this.pickUpType = -1;
+        }
+
+        try{
+            this.dropOffType = Integer.parseInt(inputArray[7]);
+        }
+        catch (Exception e){
+            this.dropOffType = -1;
+        }
+
+        try{
+            this.distance = Double.parseDouble(inputArray[8]);
+        }
+        catch (Exception e){
+            this.distance = -1;
+        }
+
+
+    }
+
+    // Getters for the data
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getStopId() {
+        return stopId;
+    }
+
+    public int getStopSequence() {
+        return stopSequence;
+    }
+
+    public int getPickUpType() {
+        return pickUpType;
+    }
+
+    public int getDropOffType() {
+        return dropOffType;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+}
---
+++ GroupAssignment/src/Transfer.java
@@ -0,0 +1,69 @@
+
+
+public class Transfer {
+
+    // data stored from transfers.txt
+    private int fromStopId;
+    private int toStopId;
+    private int transferType;
+    private int transferTime;
+
+    /**
+     *
+     * @param inputLine: single line from transfers.txt. from_stop_id,to_stop_id,transfer_type,min_transfer_time
+     */
+    public Transfer(String inputLine){
+        // split the line into its individual parameters.
+        String[] inputArray = inputLine.split(",");
+
+        // assign the data to variables
+        try{
+            this.fromStopId = Integer.parseInt(inputArray[0]);
+        }
+        catch (Exception e){
+            this.fromStopId = -1;
+        }
+
+        try{
+            this.toStopId = Integer.parseInt(inputArray[1]);
+        }
+        catch (Exception e){
+            this.fromStopId = -1;
+        }
+
+        try{
+            this.transferType = Integer.parseInt(inputArray[2]);
+        }
+        catch (Exception e){
+            this.transferType = -1;
+        }
+
+        try{
+            this.transferTime = Integer.parseInt(inputArray[3]);
+        }
+        catch (Exception e){
+            this.transferTime = -1;
+        }
+
+
+    }
+
+    // Getter for the stored data.
+
+    public int getFromStopId() {
+        return fromStopId;
+    }
+
+    public int getToStopId() {
+        return toStopId;
+    }
+
+    public int getTransferTime() {
+        return transferTime;
+    }
+
+    public int getTransferType() {
+        return transferType;
+    }
+
+}
